35a099870cf2c5ea557adb55e5010bcb7860f5da
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2022-04-12 14:58:31 -0300

    Redirect funcionando

--- react-tests/src/pages/login/login.js
+++ react-tests/src/pages/login/login.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { resolvePath } from 'react-router-dom'
 import '../../pages/login/login.css'
 
 function Login() {
     const [userName, setUserName] = useState('')
-    const [password, setPassword] = useState('')
+    const [password, setUserPassword] = useState('')
 
-    function handleLogin() {
+    const handleLogin = (event) => {
+        event.preventDefault()
+        console.log('userName: ', userName)
+        console.log('password: ', password)
         if (userName === 'admin' && password === 'admin') {
-            return <Link to='home' />
+            console.log('Login success')
+            window.location.href = '/home'
         }
     }
 
@@ -19,21 +23,21 @@ function Login() {
                 <form>
                     <div>
                         <label className='custom-field'>
-                            <input onChange={event => setUserName(event.target.value)} type='text' required />
-                            <span class="placeholder">Username</span>
+                            <input name='userName' type='text' onChange={event => setUserName(event.target.value)} required />
+                            <span className='placeholder'>Username</span>
                         </label>
                     </div>
                     <div>
                         <label className='custom-field'>
-                            <input onChange={event => setPassword(event.target.value)} type='password' required />
-                            <span class="placeholder">Password</span>
+                            <input name='password' type='password' onChange={event => setUserPassword(event.target.value)} required />
+                            <span className='placeholder'>Password</span>
                         </label>
                     </div>
-                    <button onClick={handleLogin}>Login</button>
+                    <button type='submit' onClick={handleLogin}>Login</button>
                 </form>
             </div>
         </div>
-    );
+    )
 }
 
 export default Login;
